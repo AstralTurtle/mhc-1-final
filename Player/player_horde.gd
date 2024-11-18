535fefc2868cd97e7270f7deff20256274cb865b
Aaron J <jamesacs329@gmail.com>
2024-11-18 15:54:17 -0500

    mouse and touch input working

--- Player/player_horde.gd
+++ Player/player_horde.gd
@@ -1,29 +1,25 @@
 extends Node2D
 
-@onready var target_location: int = position.x
+@onready var target_location: float = position.x
 
 @export var speed: float = 100.0
 
-@onready var cam: Camera2D = get_tree().root.get_node('World/Camera2D')
+@onready var cam: Camera2D = get_viewport().get_camera_2d()
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-# Debug the mouse position
-#func _draw():
-	#draw_circle(get_viewport().get_mouse_position(), 5, Color.RED)
-	#draw_circle(cam.get_screen_to_world(get_viewport().get_mouse_position()), 5 , Color.BLUE)
-
+@onready var view_to_world = get_canvas_transform().affine_inverse()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	global_position.x = move_toward(global_position.x, target_location, speed)
-	#_draw()
+	global_position.x = clampf(global_position.x, -600, 600) # slightly larger then the border, but prevents the horde from taking to long to respond
+	#print(global_position.x)
 
+# read mouse/ touch input -Aaron
 func _input(event: InputEvent) -> void:
 	if event is InputEventScreenDrag:
-		target_location = event.position.x
+		target_location = (view_to_world * event.position).x
 	if event is InputEventMouseMotion:
 		target_location = get_global_mouse_position().x
+	
+	
 		
