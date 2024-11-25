1318834913fbe742af3c72ea0abd30f747a60ad9
WesleyW8542 <141263094+WesleyW8542@users.noreply.github.com>
2024-11-25 03:46:19 +0000

    added bullets for player

--- Player/player_horde.gd
+++ Player/player_horde.gd
@@ -8,6 +8,10 @@ extends Node2D
 
 @onready var view_to_world = get_canvas_transform().affine_inverse()
 
+@onready var bullet = preload("res://Player/bullet.tscn")
+
+var time_accumulated = 0 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	global_position.x = move_toward(global_position.x, target_location, speed)
@@ -21,5 +25,14 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		target_location = get_global_mouse_position().x
 	
+func _process(delta):
+	time_accumulated += delta
+	#adjust when player gets buffs
+	var rate = 0.1
+	if(time_accumulated >= rate):
+		var bullets = bullet.instantiate()
+		bullets.position = position
+		bullets.position.y -= 125
+		get_parent().add_child(bullets)
+		time_accumulated = 0
 	
-		
