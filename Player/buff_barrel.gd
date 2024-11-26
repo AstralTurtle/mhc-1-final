e6d77be5078f27052cd62a2b078454f9593d8979
Aaron J <jamesacs329@gmail.com>
2024-11-26 12:41:25 -0500

    working on buffs

---
+++ Player/buff_barrel.gd
@@ -0,0 +1,32 @@
+extends Area2D
+class_name  BuffBarrel
+var health: float = 10
+
+var speed: float = 100
+
+@onready var buffTexture: Sprite2D = get_node('BuffTexture')
+@onready var HPDisplay: Label = $Label
+
+func _ready() -> void:
+    area_entered.connect(areaEntered)
+    
+func setTexture(tex: Texture2D):
+    print(buffTexture)
+    # buffTexture.texture = tex
+
+func _physics_process(delta: float):
+    position.y += speed * delta
+    HPDisplay.text = str(health)
+
+func areaEntered(area: Area2D):
+    if area.is_in_group('player_projectile'):
+        var proj: PlayerProjectile = area as PlayerProjectile
+        if proj.pierce:
+            proj.damage -= health
+        else: 
+            proj.damage = 0
+        health -= proj.damage
+        print('ball')
+        if health <= 0:
+            queue_free()        
+
--- Player/player_horde.gd
+++ Player/player_horde.gd
@@ -1,4 +1,10 @@
 extends Node2D
+class_name PlayerHordeController
+
+
+var hoard_count: int = 0
+var hoard_level: int = 1
+const player_hoard_group: String = "_PlayerHoardGroup"
 
 @onready var target_location: float = position.x
 
@@ -8,18 +14,41 @@ extends Node2D
 
 @onready var view_to_world = get_canvas_transform().affine_inverse()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@export var horde_member: PackedScene
+
+
 func _physics_process(delta: float) -> void:
 	global_position.x = move_toward(global_position.x, target_location, speed)
 	global_position.x = clampf(global_position.x, -600, 600) # slightly larger then the border, but prevents the horde from taking to long to respond
-	#print(global_position.x)
 
-# read mouse/ touch input -Aaron
 func _input(event: InputEvent) -> void:
 	if event is InputEventScreenDrag:
 		target_location = (view_to_world * event.position).x
 	if event is InputEventMouseMotion:
 		target_location = get_global_mouse_position().x
+	if event.is_action_pressed('debug'):
+		spawn()
+
+func _ready() -> void:
+	spawn()
+
 	
+func spawn():
+	print('spawning')
+	var new_member: PlayerHordeMember = horde_member.instantiate()
+	new_member.health *=  (hoard_level * 10)
+	new_member.damage *= (hoard_level * 10)
+	new_member.position = position + Vector2(randi_range(-10,10),randi_range(-20,20))
+	new_member.add_to_group(player_hoard_group)
+	get_parent().add_child(new_member)
+	hoard_count += 1
+	merge()
+
+func merge():
+	if hoard_count > 10:
+		hoard_level += 1
+		hoard_count = 0
+		get_tree().call_group(player_hoard_group, 'queue_free')
+		spawn()
 	
 		
