60c1f282f74b60d7d775d4e92c9efefd16d64d19
WesleyW8542 <141263094+WesleyW8542@users.noreply.github.com>
2024-12-02 06:17:40 +0000

    added enemy and interactions

--- enemy/enemy_spawner.gd
+++ enemy/enemy_spawner.gd
@@ -1,10 +1,10 @@
 extends Node2D
-@export var enemies: Array[PackedScene] = []
+
 var difficulty: float = 0;
 @export var increaseDifficultyTime: float= 30
 @export var difficultyIncreaseAmount: float = 0.5
 @export var spawnTime: float = 15;
-var timer: float = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,9 +20,4 @@ func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	timer += delta
-	if (timer > spawnTime - difficulty) :
-		for i in range(int(1 + difficulty)):
-			spawn()
-	if (timer > increaseDifficultyTime):
-		difficulty += difficultyIncreaseAmount
+	pass
