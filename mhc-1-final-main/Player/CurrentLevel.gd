db619554aed549340fa735d30271c67ab5be1b10
WesleyW8542 <141263094+WesleyW8542@users.noreply.github.com>
2024-12-02 01:02:49 -0500

    oops

--- mhc-1-final-main/Player/CurrentLevel.gd
+++
@@ -1,6 +0,0 @@
-extends Label
-
-@onready var controller: PlayerHordeController = get_tree().root.get_node("World/PlayerHorde/PlayerHordeController")
-
-func _process(delta: float) -> void:
-	text = 'Current Level: ' + str(controller.hoard_level)
