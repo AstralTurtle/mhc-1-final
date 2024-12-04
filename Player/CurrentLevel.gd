054108bd24e31c82b662c701c9c653a30705a08c
Aaron J <jamesacs329@gmail.com>
2024-12-03 22:36:17 -0500

    add mascot img

--- Player/CurrentLevel.gd
+++ Player/CurrentLevel.gd
@@ -3,4 +3,4 @@ extends Label
 @onready var controller: PlayerHordeController = get_tree().root.get_node("World/PlayerHorde/PlayerHordeController")
 
 func _process(delta: float) -> void:
-	text = 'Current Level: ' + str(controller.hoard_level)
+	text = 'Merge Level: ' + str(controller.hoard_level)
--- Player/player_horde_member.gd
+++ Player/player_horde_member.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 class_name PlayerHordeMember
 @onready var center: PlayerHordeController  = get_parent().get_node('PlayerHordeController')
-
+@export var sprites: Array[Texture2D] = []
 @export var speed: float =  100 
 
 @export var health: float = 5
@@ -25,6 +25,10 @@ func take_damage(damage: float):
 	health -= damage
 	if health < 0:
 		queue_free()
+
+func _ready() -> void:
+	$Sprite2D.texture = sprites[randi_range(0,sprites.size()-1)]
+
 
 func shoot():
 	var proj = center.projectile.instantiate() as PlayerProjectile
